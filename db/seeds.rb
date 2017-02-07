b4d351d89715e6ffd15feb2fbf6593fa8f0b8427
Cliff Harris <csharris5@uh.edu>
2017-02-06 18:06:35 -0600

    fixed seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 Assignment.create!([
-  {name: "Introduction to Cloud Computing", course_module_id: 1, assignment_source: "Eli the Computer Guy", assignment_file: "Introduction_to_Cloud_Computing.pdf"}
+  {name: "Introduction to Cloud Computing", course_module_id: 1, assignment_source: "Eli the Computer Guy", assignment_file: "Introduction_to_Cloud_Computing.pdf"},
+  {name: "NIST Definition of Cloud Computing", course_module_id: 1, assignment_source: "NIST SP800-145", assignment_file: "NIST_SP800-145.pdf"},
+  {name: "NIST Cloud Computing Synopsis and Recommendations", course_module_id: 1, assignment_source: "NIST SP800-146", assignment_file: "NIST_SP800-146.pdf"}
 ])
 Course.create!([
   {course_num: "CIS 3357", name: "Cloud Computing Infrastructure"},
