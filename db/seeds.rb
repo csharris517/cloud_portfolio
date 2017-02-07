1defcfc814d71f76932d27c142748aa5d37a678b
Cliff Harris <csharris5@uh.edu>
2017-02-06 18:09:44 -0600

    fix order

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,3 @@
-Assignment.create!([
-  {name: "Introduction to Cloud Computing", course_module_id: 1, assignment_source: "Eli the Computer Guy", assignment_file: "Introduction_to_Cloud_Computing.pdf"},
-  {name: "NIST Definition of Cloud Computing", course_module_id: 1, assignment_source: "NIST SP800-145", assignment_file: "NIST_SP800-145.pdf"},
-  {name: "NIST Cloud Computing Synopsis and Recommendations", course_module_id: 1, assignment_source: "NIST SP800-146", assignment_file: "NIST_SP800-146.pdf"}
-])
 Course.create!([
   {course_num: "CIS 3357", name: "Cloud Computing Infrastructure"},
 ])
@@ -11,4 +6,9 @@ CourseModule.create!([
   {module_num: "Module 2", course_id: 1},
   {module_num: "Module 3", course_id: 1},
   {module_num: "Module 4", course_id: 1}
+])
+Assignment.create!([
+  {name: "Introduction to Cloud Computing", course_module_id: 1, assignment_source: "Eli the Computer Guy", assignment_file: "Introduction_to_Cloud_Computing.pdf"},
+  {name: "NIST Definition of Cloud Computing", course_module_id: 1, assignment_source: "NIST SP800-145", assignment_file: "NIST_SP800-145.pdf"},
+  {name: "NIST Cloud Computing Synopsis and Recommendations", course_module_id: 1, assignment_source: "NIST SP800-146", assignment_file: "NIST_SP800-146.pdf"}
 ])
